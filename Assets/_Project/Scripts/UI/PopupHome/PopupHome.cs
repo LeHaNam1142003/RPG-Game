9c13a9595a161fa98f524087c60023e35b06a245
LeHaNam1142003 <110580153+LeHaNam1142003@users.noreply.github.com>
2023-05-16 10:27:02 +0700

    Initial commit

---
+++ Assets/_Project/Scripts/UI/PopupHome/PopupHome.cs
@@ -0,0 +1,46 @@
+using System.Reflection;
+
+public class PopupHome : Popup
+{
+    protected override void BeforeShow()
+    {
+        base.BeforeShow();
+        PopupController.Instance.Show<PopupUI>();
+    }
+    
+    protected override void BeforeHide()
+    {
+        base.BeforeHide();
+        PopupController.Instance.Hide<PopupUI>();
+    }
+
+    public void OnClickStart()
+    {
+        GameManager.Instance.StartGame();
+    }
+
+    public void OnClickDebug()
+    {
+        PopupController.Instance.Show<PopupDebug>();
+    }
+
+    public void OnClickSetting()
+    {
+        PopupController.Instance.Show<PopupSetting>();
+    }
+
+    public void OnClickDailyReward()
+    {
+        PopupController.Instance.Show<PopupDailyReward>();
+    }
+
+    public void OnClickShop()
+    {
+        PopupController.Instance.Show<PopupShop>();
+    }
+    
+    public void OnClickTest()
+    {
+        PopupController.Instance.Show<PopupTest>();
+    }
+}
